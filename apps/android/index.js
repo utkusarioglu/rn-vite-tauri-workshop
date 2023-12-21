60cbdde43ce3ff7c72a096d30799edd205d296f2
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-12-21 11:42:25 +0000

    Fix: Startup issues in `android` and `spa`

--- apps/android/index.js
+++ apps/android/index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from "react-native";
-import RnApp from "./src/RnApp";
+import { AndroidApp } from "./src/AndroidApp";
 import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => RnApp);
+AppRegistry.registerComponent(appName, () => AndroidApp);
--- apps/android/src/AndroidApp.tsx
+++ apps/android/src/AndroidApp.tsx
@@ -8,29 +8,29 @@
 import React from "react";
 import type { PropsWithChildren } from "react";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
-  View,
+  SafeAreaView,
+  // ScrollView,
+  // StatusBar,
+  // StyleSheet,
+  // Text,
+  // View,
 } from "react-native";
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  // DebugInstructions,
+  // Header,
+  // LearnMoreLinks,
+  // ReloadInstructions,
 } from "react-native/Libraries/NewAppScreen";
 import { XpApp } from "xp-app";
 import { ElementsProvider } from "elements";
 import androidTamaguiConfig from "../tamagui.config";
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+// type SectionProps = PropsWithChildren<{
+//   title: string;
+// }>;
 
 // function Section({ children, title }: SectionProps): React.JSX.Element {
 //   const isDarkMode = useColorScheme() === "dark";
@@ -62,7 +62,7 @@ type SectionProps = PropsWithChildren<{
  * @dev
  * 1- TODO Tamagui type mismatch
  */
-function RnApp(): React.JSX.Element {
+export function AndroidApp(): React.JSX.Element {
   const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
@@ -107,23 +107,23 @@ function RnApp(): React.JSX.Element {
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-});
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: "600",
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: "400",
+//   },
+//   highlight: {
+//     fontWeight: "700",
+//   },
+// });
 
-export default RnApp;
+// export default AndroidApp;
